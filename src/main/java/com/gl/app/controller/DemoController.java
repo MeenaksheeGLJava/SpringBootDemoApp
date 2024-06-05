5da09b71890cd0971401519050ffd7dc5c76d334
Minakshi Sharma <minakshi.sharma@globallogic.com>
2024-06-05 14:36:37 +0530

    feature branch added

--- src/main/java/com/gl/app/controller/DemoController.java
+++ src/main/java/com/gl/app/controller/DemoController.java
@@ -9,7 +9,7 @@ public class DemoController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		System.out.println("hello from spring boot");
-		System.out.println("Hi hello from spring boot");
+		System.out.println("Hi hello from from from spring boot");
 		return "Hello from Spring Boot!";
 	}
 
